67b9e67a21d00d09c57ca4192c3bd4c178259b2e
Tomás Coêlho <tomascoelho6@gmail.com>
2021-08-16 13:22:27 -0300

    Add MVI on Brazil dashboard

---
+++ mvi_in_brazil.js
@@ -0,0 +1,44 @@
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(main);
+
+async function main() {
+  let dataset = await d3
+    .csv('data/mvi_brazil.csv')
+    .then(data => {
+      data.forEach(item => {
+        item.homicidesRatio = parseFloat(item.homicidesRatio);
+      });
+      return data;
+    });
+
+  facts = crossfilter(dataset);
+
+
+  (function(facts) {
+    let locationDimension = facts.dimension(item => item.location);
+    let homicidesByLocation = locationDimension.group().reduceSum(item => item.homicidesRatio);
+    let locationNames = facts.all()
+      .map(item => item.location);
+    debugger;
+
+    let barChart = dc.barChart(document.querySelector('#cvli-in-brazil'));
+
+    barChart
+      .height(300)
+      .dimension(locationDimension)
+      .x(d3.scaleOrdinal().domain(locationNames))
+      .xUnits(dc.units.ordinal)
+      .renderHorizontalGridLines(true)
+      .group(homicidesByLocation)
+
+  })(facts);
+
+  dc.renderAll();
+}
