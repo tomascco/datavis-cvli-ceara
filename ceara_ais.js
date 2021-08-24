fe2627dc3dddf24e49f7ffc3009606046f80ac55
Tomás Coêlho <tomascoelho6@gmail.com>
2021-08-24 07:53:51 -0300

    Fix clear filter button and remove some whitespace

--- ceara_ais.js
+++ ceara_ais.js
@@ -9,32 +9,32 @@ ready(main);
 let mapLayerGroup =[];
 let map;
 function showLayer(municipios){
-  for(let i=0;i<municipios.length;i++){  
+  for(let i=0;i<municipios.length;i++){
     var lg = mapLayerGroup[municipios[i]];
     if(!(typeof lg === 'undefined')){
     map.addLayer(lg);
-    } 
-   }   
+    }
+  }
 }
 function hideLayer(municipios){
-  for(let i=0;i<municipios.length;i++){  
+  for(let i=0;i<municipios.length;i++){
     var lg = mapLayerGroup[municipios[i]];
     if(!(typeof lg === 'undefined')){
     console.log(lg)
     map.removeLayer(lg);
-    }  
+    }
 }
 }
 
 async function updateMarkers(idGroup,mun_ais){
-    
+
   let ids = idGroup.all()
   //console.log(ids)
   let todisplay = new Array(ids.length) //preallocate array to be faster
   let mc = 0; //counter of used positions in the array
   for (let i = 0; i < ids.length; i++) {
-  let tId = ids[i]; 
-  if(tId.value > 0){ 
+  let tId = ids[i];
+  if(tId.value > 0){
       //when an element is filtered, it has value > 0
       todisplay[mc] =tId.key[1]
       mc = mc + 1
@@ -49,14 +49,14 @@ async function updateMarkers(idGroup,mun_ais){
   }
   let municipios_selected = []
   let muncipios_removed = []
-  
+
   var data_mun = await d3.csv('data/MUNICIPIO_AIS_interior.csv')
   data_mun.forEach(function(item){
                     if(ais_groups.indexOf(item.AIS) > -1){municipios_selected.push(item.MUNICIPIO)}
                     if(!(ais_groups.indexOf(item.AIS) > -1)){muncipios_removed.push(item.MUNICIPIO)}
                     })
   console.log(municipios_selected)
-  hideLayer(muncipios_removed) 
+  hideLayer(muncipios_removed)
   showLayer(municipios_selected)
 }
 
@@ -88,7 +88,7 @@ async function weaponControls(facts) {
     .group(weaponDimension.group())
 }
 function rowChart(facts){
-  
+
   let ais_dim = facts.dimension(d=>d.AIS);
   let ais_group = ais_dim.group();
   let ais_labels = ['AIS 11','AIS 12','AIS 13','AIS 14','AIS 15','AIS 16','AIS 17','AIS 18','AIS 19','AIS 20','AIS 21','AIS 22']
@@ -97,7 +97,7 @@ function rowChart(facts){
   let crime_scale = d3.scaleSequential().domain([0, 500]).interpolator(d3.interpolateReds)
   let xScale_ais = d3.scaleOrdinal().domain(ais_labels)
   let rChart = dc.rowChart(document.querySelector("#bar_ais"));
-  
+
   rChart
       .height(400)
       .width(400)
@@ -118,16 +118,16 @@ function rowChart(facts){
                 .attr("x", chartToUpdate.width()/2)
                 .attr("y", chartToUpdate.height()-4.5)
                 .text(displayText);
-                
+
   }
     AddXAxis(rChart, "Taxa de CVLI por 100 mill");
 }
 function histogram_ceara(facts){
   ageDimension = facts.dimension(d => d.IDADE);
     ageCount = ageDimension.group().reduceCount();
-  
+
     let histogram = dc.barChart("#hist_ais");
-  
+
     histogram
       .height(400)
       .dimension(ageDimension)
@@ -135,7 +135,7 @@ function histogram_ceara(facts){
       .group(ageCount)
       .x(d3.scaleLinear().domain([0, 100]))
       .elasticY(true);
-  
+
     histogram.xAxisLabel("Idade");
     histogram.yAxisLabel("Count");
 }
@@ -175,12 +175,6 @@ async function renderMap_ceara(facts) {
   let cityDimension = facts.dimension(d => d.MUNICIPIO);
   let CITY_GROUP_BY = cityDimension.group().all();
 
-  d3.select('#clear-all').on('click', () => {
-    cityDimension.filterAll();
-    dc.redrawAll();
-    map.setView([-4.8864139104811946, -39.60018165919775], 7);
-  });
-
   let map_tax = new Map();
   let map_numbers =new Map();
   let ais_map_tax = new Map();
@@ -216,7 +210,7 @@ async function renderMap_ceara(facts) {
 
       return map_mun;
   });
-  console.log(ais_map_tax)  
+  console.log(ais_map_tax)
   let geoj = L.geoJson(geo_mun,{
     style: style_ceara,
     onEachFeature: onEachFeature
@@ -293,17 +287,17 @@ async function renderMap_ceara(facts) {
   }
   function onEachFeature(feature, layer) {
       var lg = mapLayerGroup[feature.properties.name];
-  
+
       if (lg === undefined) {
           lg = new L.layerGroup();
           //add the layer to the map
-          
+
           lg.addTo(map);
-          
+
           //store layer
           mapLayerGroup[feature.properties.name] = lg;
       }
-  
+
       //add the feature to the layer
       layer.on({
             mouseover: highlightFeature,
--- fortaleza_ais.js
+++ fortaleza_ais.js
@@ -10,36 +10,36 @@ function ready(fn) {
   let dataset2_fortaleza;
   let crime_scale2;
   //let facts;
-  
+
   ready(main);
   function showLayer2(municipios){
-    for(let i=0;i<municipios.length;i++){  
+    for(let i=0;i<municipios.length;i++){
       var lg = mapLayerGroups2[municipios[i]];
       if(!(typeof lg === 'undefined')){
       map2.addLayer(lg);
-      } 
-     }   
+      }
+    }
   }
-  
+
   function hideLayer2(municipios){
-    for(let i=0;i<municipios.length;i++){  
+    for(let i=0;i<municipios.length;i++){
       var lg = mapLayerGroups2[municipios[i]];
       if(!(typeof lg === 'undefined')){
       console.log(lg)
       map2.removeLayer(lg);
-      }  
+      }
   }
   }
-  
+
   function updateMarkers2(idGroup,bairros_AIS){
-    
+
     let ids = idGroup.all()
     //console.log(ids)
     let todisplay = new Array(ids.length) //preallocate array to be faster
     let mc = 0; //counter of used positions in the array
     for (let i = 0; i < ids.length; i++) {
-    let tId = ids[i]; 
-      if(tId.value > 0){ 
+    let tId = ids[i];
+      if(tId.value > 0){
         //when an element is filtered, it has value > 0
         todisplay[mc] =tId.key[1]
         mc = mc + 1
@@ -47,7 +47,7 @@ function ready(fn) {
     }
     //console.log(todisplay)
     let ais_groups =[];
-  
+
     for(let i=0;i<todisplay.length;i++){
       if(!(ais_groups.indexOf(todisplay[i]) > -1)){
         ais_groups.push(todisplay[i])
@@ -60,62 +60,59 @@ function ready(fn) {
                       if(!(ais_groups.indexOf(item.AIS) > -1)){bairros_removed.push(item.Nome)}
                       })
 
-    hideLayer2(bairros_removed) 
+    hideLayer2(bairros_removed)
     showLayer2(bairros_selected)
   }
-  
+
   async function genderControls3(facts) {
     genderDimension = facts.dimension(d => d.SEXO);
     let genderControls = dc.cboxMenu('#gender-controls_fortaleza');
-  
+
     genderControls
       .dimension(genderDimension)
       .group(genderDimension.group())
       .multiple(true);
   }
-  
+
   async function crimeControls3(facts) {
     crimeDimension = facts.dimension(d => d['NATUREZA DO FATO']);
     let kindOfCrimeControls = dc.cboxMenu('#kind-of-crime_fortaleza');
-  
+
     kindOfCrimeControls
       .dimension(crimeDimension)
       .group(crimeDimension.group())
   }
-  
+
   async function weaponControls3(facts) {
     weaponDimension = facts.dimension(d => d['ARMA-UTILZADA']);
     let weaponControls = dc.cboxMenu('#weapon-controls_fortaleza');
-  
+
     weaponControls
       .dimension(weaponDimension)
       .group(weaponDimension.group())
   }
-  
+
   async function updateData2() {
-    
-    
-    
     let ais_dim = facts_fortaleza.dimension(d=>d.AIS)
     let ais_group = ais_dim.group()
     let max_value = 0
     let map_AIS_count = new Map()
-  
+
     ais_group.all().forEach(function(item){
       if(max_value<item.value){max_value=item.value}
       map_AIS_count.set(item.key,+item.value)
     })
     let min_value =max_value
-    
+
     ais_group.all().forEach(function(item){
       if(min_value>item.value){min_value=item.value}
-    }) 
+    })
     crime_scale2 = d3.scaleQuantize().domain([0, max_value+20]).range(d3.schemeReds[9])
-  
-    var ais_groups=['AIS 1', 'AIS 2', 'AIS 3', 'AIS 4', 'AIS 5', 'AIS 6', 'AIS 7', 'AIS 8', 
+
+    var ais_groups=['AIS 1', 'AIS 2', 'AIS 3', 'AIS 4', 'AIS 5', 'AIS 6', 'AIS 7', 'AIS 8',
                     'AIS 9', 'AIS 10']
-    
-    
+
+
     let crime_scale2 = d3.scaleSequential().domain([min_value-10, max_value+20])
     .interpolator(d3.interpolateReds)
     lineplot_2(facts_fortaleza);
@@ -125,12 +122,11 @@ function ready(fn) {
     histogram3(facts_fortaleza);
     heatmapdays(facts_fortaleza);
     document.getElementById("demo2").innerHTML = "<b>Mapa e gráfico de barra estão considerando os dados Brutos</b>";
-  
+
     dc.renderAll();
   }
-  
+
   async function originalData2(){
-    
     AIS_pop = await d3.csv('data/AIS_pop.csv').then(
         function(data){
         let map_AIS_pop = new Map()
@@ -146,40 +142,40 @@ function ready(fn) {
     let ais_group = ais_dim.group()
     let max_value = 0
     let map_AIS_count = new Map()
-  
-    
+
+
     ais_group.all().forEach(function(item){
       if(max_value<item.value*100000/AIS_pop.get(item.key)){max_value=item.value*100000/AIS_pop.get(item.key)}
       item.value = item.value*100000/AIS_pop.get(item.key)
       map_AIS_count.set(item.key,item.value)
     })
     let min_value =max_value
-    
+
     ais_group.all().forEach(function(item){
       if(min_value>item.value*100000/AIS_pop.get(item.key)){min_value=item.value*100000/AIS_pop.get(item.key)}
     })
-  
-    let ais_groups=['AIS 11','AIS 12', 'AIS 13', 'AIS 14', 'AIS 15', 'AIS 16', 'AIS 17', 'AIS 18', 'AIS 19', 'AIS 20', 
+
+    let ais_groups=['AIS 11','AIS 12', 'AIS 13', 'AIS 14', 'AIS 15', 'AIS 16', 'AIS 17', 'AIS 18', 'AIS 19', 'AIS 20',
                     'AIS 21', 'AIS 22']
-    
-    
+
+
     let crime_scale2 = d3.scaleSequential().domain([min_value-10, max_value+20])
     .interpolator(d3.interpolateReds)
     map2.remove();
     renderMap3(facts_fortaleza,crime_scale2,AIS_pop,map_AIS_count,AIS_MUN);
     horizontalbar2(facts_fortaleza,ais_groups,AIS_df,AIS_pop,crime_scale2,ais_group,ais_dim)
     document.getElementById("demo2").innerHTML = "</b>Mapa e gráfico de barra estão considerando a Taxa de CVLI por 100.000 mill habitantes</b>";
-  
+
     dc.renderAll();
   }
 
-  
+
   function histogram3(facts){
     ageDimension = facts.dimension(d => d.IDADE);
     ageCount = ageDimension.group().reduceCount();
-  
+
     let histogram = dc.barChart("#hist_fortaleza");
-  
+
     histogram
       .height(400)
       .dimension(ageDimension)
@@ -187,18 +183,18 @@ function ready(fn) {
       .group(ageCount)
       .x(d3.scaleLinear().domain([0, 100]))
       .elasticY(true);
-  
+
     histogram.xAxisLabel("Idade");
     histogram.yAxisLabel("Count");
-  
+
   }
-  
+
   function heatmapdays2(facts){
     var sexDaydim_2 = facts.dimension(function(item){return [item.Mes,item.DiaDaSemana]})
     var sexDayGroup_2 = sexDaydim_2.group();
     let heatmap_fortaleza = new dc.HeatMap("#heatmap_fortaleza")
     let dias = ['Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo']
-  
+
     heatmap_fortaleza
     .height(400)
     .width(450)
@@ -216,14 +212,14 @@ function ready(fn) {
     .calculateColorDomain();
   }
   function horizontalbar2(facts_fortaleza,ais_groups,bairros_AIS,AIS_pop,crime_scale2,ais_group,ais_dim){
-  
-    
+
+
     let idDim = facts_fortaleza.dimension(d=>[d.ID,d.AIS])
     let idGroup= idDim.group()
-    
+
     let xScale_ais = d3.scaleOrdinal().domain(ais_groups)
     let rChart = dc.rowChart(document.querySelector("#bar_fortaleza"));
-    
+
     rChart
         .height(400)
         .width(400)
@@ -244,73 +240,67 @@ function ready(fn) {
                   .attr("x", chartToUpdate.width()/2)
                   .attr("y", chartToUpdate.height()-4.5)
                   .text(displayText);
-                  
+
     }
       AddXAxis(rChart, "Taxa de CVLI por 100 mill");
   }
-  
+
   async function  renderMap3(facts,crime_scale2,AIS_pop,map_AIS_count,bairro_AIS_map){
     let pop_mun = await d3
       .csv("data/pop_est@3.csv", item => {
         item.POP = parseInt(item.POP);
-  
+
         return item;
       })
       .then(function(data){
         let map_mun = new Map();
-  
+
         data.forEach(item => {
           map_mun.set(item["NOME"], item["POP"])
         });
-  
+
         return map_mun;
       });
-  
+
     let cityDimension = facts.dimension(d => d.MUNICIPIO);
     let CITY_GROUP_BY = cityDimension.group().all();
-  
-    d3.select('#clear-all').on('click', () => {
-      cityDimension.filterAll();
-      dc.redrawAll();
-      map.setView([-3.792614,-38.515877], 11);
-    });
-  
+
     let geo_mun = await d3.json("data/FortalezaBairros.geojson");
-  
+
     map2 = L.map('ais_fortaleza').setView([-3.792614,-38.515877], 11)
-  
+
     L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
                   attribution:`&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,
-                  Map tiles by &copy; <a href="https://carto.com/attribution">CARTO</a>`,     
+                  Map tiles by &copy; <a href="https://carto.com/attribution">CARTO</a>`,
                   maxZoom:17}).addTo(map2)
-  
+
     let infoControl = L.control();
-  
+
     let geoj = L.geoJson(geo_mun,{
       style: style,
       onEachFeature: onEachFeature2
     }).addTo(map2)
-  
+
     infoControl.onAdd = function (map2) {
       this._div = L.DomUtil.create('div', 'info');
       this.update();
       return this._div;
       }
-  
+
     infoControl.update = function (feat) {
       }
-  
+
     infoControl.addTo(map2);
-  
+
     function zoomToFeature(e) {
       cityDimension.filterExact(e.target.feature.properties.name);
       map2.fitBounds(e.target.getBounds());
-  
+
       dc.redrawAll();
     }
     function onEachFeature2(feature, layer) {
       var lg = mapLayerGroups2[feature.properties.NOME];
-  
+
       if (lg === undefined) {
           lg = new L.layerGroup();
           //add the layer to the map
@@ -318,7 +308,7 @@ function ready(fn) {
           //store layer
           mapLayerGroups2[feature.properties.NOME] = lg;
     }
-  
+
       //add the feature to the layer
       layer.on({
             mouseover: highlightFeature,
@@ -329,28 +319,28 @@ function ready(fn) {
     }
     function highlightFeature(e) {
       let layer = e.target;
-  
+
       layer.setStyle({
             weight: 2,
             color: '#fcba03',
             dashArray: '',
             fillOpacity: 0.7
       });
-      layer.bindTooltip( 
+      layer.bindTooltip(
         '<b>' + layer.feature.properties.NOME +'</br>'+bairro_AIS_map.get(layer.feature.properties.NOME)+ '</b><br />').openTooltip();
-  
+
       if (!L.Browser.ie && !L.Browser.opera) {
         layer.bringToFront();
       }
-  
+
       infoControl.update(layer.feature);
     }
-  
+
     function resetHighlight(e) {
       geoj.resetStyle(e.target);
       infoControl.update();
     }
-    
+
     function style(feature) {
         return {
               weight: 0,
@@ -366,17 +356,17 @@ function ready(fn) {
       }
       function onEachFeature2(feature, layer) {
         var lg = mapLayerGroups2[feature.properties.NOME];
-    
+
         if (lg === undefined) {
             lg = new L.layerGroup();
             //add the layer to the map
-            
+
             lg.addTo(map2);
-            
+
             //store layer
             mapLayerGroups2[feature.properties.NOME] = lg;
         }
-    
+
         //add the feature to the layer
         layer.on({
               mouseover: highlightFeature,
@@ -385,17 +375,17 @@ function ready(fn) {
             });
         lg.addLayer(layer);
       }
-      
+
   }
-  
-  
+
+
   function lineplot_3(facts){
     let SeriesDim = facts.dimension(d => d3.timeMonth(d.dtg));
-  
+
     let monthScale = d3.scaleTime().domain([ SeriesDim.bottom(1)[0].dtg, SeriesDim.top(1)[0].dtg]);
-  
+
     let lineChart = dc.lineChart(document.querySelector('#vitimas_fortaleza'));
-    
+
     lineChart
       .height(400)
       .width(800)
@@ -405,7 +395,7 @@ function ready(fn) {
       .x(monthScale)
       .renderDataPoints(true)
       .elasticY(true);
-  
+
       lineChart.xAxisLabel("Data (dia)");
       lineChart.yAxisLabel("Número de CVLI");
   }
@@ -417,16 +407,16 @@ function ready(fn) {
 
       data.forEach(function(item){
         item.dtg = parseDate(item.DATA);
-        
+
       });
-    
+
       return data;
   });
 
   let dataset2_fortaleza= [];
 
   let fil_dat=dataset2.filter(function(d) {
-  
+
   return d.MUNICIPIO.indexOf('Fortaleza') !== -1
   })
 
@@ -435,7 +425,7 @@ function ready(fn) {
   dataset2_fortaleza.forEach(function(d){
     d.ID=count;count=count+1;
   })
-  let ais_groups=['AIS 1', 'AIS 2', 'AIS 3', 'AIS 4', 'AIS 5', 'AIS 6', 'AIS 7', 
+  let ais_groups=['AIS 1', 'AIS 2', 'AIS 3', 'AIS 4', 'AIS 5', 'AIS 6', 'AIS 7',
                     'AIS 8', 'AIS 9', 'AIS 10']
   AIS_pop = await d3.csv('data/AIS_pop.csv').then(
         function(data){
@@ -454,22 +444,18 @@ function ready(fn) {
     let ais_group = ais_dim.group()
     let max_value = 0
     let map_AIS_count = new Map()
-  
+
     ais_group.all().forEach(function(item){
       if(max_value<item.value*100000/AIS_pop.get(item.key)){max_value=item.value*100000/AIS_pop.get(item.key)}
       item.value = item.value*100000/AIS_pop.get(item.key)
       map_AIS_count.set(item.key,item.value)
     })
     let min_value =max_value
-    
+
     ais_group.all().forEach(function(item){
       if(min_value>item.value*100000/AIS_pop.get(item.key)){min_value=item.value*100000/AIS_pop.get(item.key)}
-    }) 
-  
-    
-  
-    
-    
+    })
+
     crime_scale2 = d3.scaleSequential().domain([min_value-10, max_value+20])
     .interpolator(d3.interpolateReds)
     lineplot_3(facts_fortaleza);
@@ -477,7 +463,7 @@ function ready(fn) {
     horizontalbar2(facts_fortaleza,ais_groups,bairros_AIS,AIS_pop,crime_scale2,ais_group,ais_dim)
     histogram3(facts_fortaleza);
     heatmapdays2(facts_fortaleza);
-  
+
     genderControls3(facts_fortaleza);
     crimeControls3(facts_fortaleza);
     weaponControls3(facts_fortaleza);
